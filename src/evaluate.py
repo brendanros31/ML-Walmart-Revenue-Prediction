a8b7ef862e4ee883798c3aede177de45dcf263db
brendanros31 <brendancarlr.code@gmail.com>
2025-07-08 00:50:20 +0530

    feat: add plot class for clean code

--- src/evaluate.py
+++ src/evaluate.py
@@ -39,34 +39,35 @@ def pred_values(model, X_test, y_test):
 
 
 
-# Plotting Actual and Predicted values
-def pred_plot(model, X_test, y_test):
-    y_pred = model.predict(X_test)   # Predictions
+class plots:
 
-    plt.figure(figsize=(10,5))
+    # Plotting Actual and Predicted values
+    def pred_plot(model, X_test, y_test):
+        y_pred = model.predict(X_test)   # Predictions
 
-    sns.histplot(y_test, label='Actual', color="skyblue", alpha=0.8, kde=True)
-    sns.histplot(y_pred, label='Predicted', color="orange", alpha=0.3, kde=True)
+        plt.figure(figsize=(10,5))
 
-    plt.title('Actual vs Predicted values - Decision Tree')
-    plt.xlabel('Ticket price')
-    plt.ylabel('Count')
+        sns.histplot(y_test, label='Actual', color="skyblue", alpha=0.8, kde=True)
+        sns.histplot(y_pred, label='Predicted', color="orange", alpha=0.3, kde=True)
 
-    plt.legend()
-    plt.show()
+        plt.title('Actual vs Predicted values - Decision Tree')
+        plt.xlabel('Ticket price')
+        plt.ylabel('Count')
+
+        plt.legend()
+        plt.show()
 
 
+    # Plotting the Predictions - Future weeks
+    def future_plot(df):
+        plt.figure(figsize=(12, 6))
+        sns.lineplot(data=df, x='Date', y='Predicted_Sales', hue='Model', marker='o')
 
-# Plotting the Predictions - Future weeks
-def future_plot(df):
-    plt.figure(figsize=(12, 6))
-    sns.lineplot(data=df, x='Date', y='Predicted_Sales', hue='Model', marker='o')
+        plt.title('Predicted Weekly Sales from Different Models', fontsize=16)
+        plt.xlabel('Date', fontsize=14)
+        plt.ylabel('Predicted Sales (Millions)', fontsize=14)
+        plt.xticks(rotation=45)
+        plt.legend(title='Model')
+        plt.grid(True)
 
-    plt.title('Predicted Weekly Sales from Different Models', fontsize=16)
-    plt.xlabel('Date', fontsize=14)
-    plt.ylabel('Predicted Sales (Millions)', fontsize=14)
-    plt.xticks(rotation=45)
-    plt.legend(title='Model')
-    plt.grid(True)
-
-    plt.show()
+        plt.show()
